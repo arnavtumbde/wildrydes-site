19c70647129e22ec65c6dfd9923aadae7edac8e8
Arnav Tumbde <133473716+arnavtumbde@users.noreply.github.com>
2025-06-10 14:32:49 +0530

    Update config.js
    USER POOL ID and Client ID of TumbdeGoCabs updated

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_Jo5EKgYs1', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1h9q1im0magboeev365603fpl4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
